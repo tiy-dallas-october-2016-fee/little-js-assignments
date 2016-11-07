11389018f3e056f93b1f989196337123d783bfde
Eric Sowell <eric.sowell@gmail.com>
2016-11-07 14:27:45 -0600

    first function lab

---
+++ labs/functions/lab-01.js
@@ -0,0 +1,13 @@
+//function goes here
+
+function sum(parm0, parm1) {
+  var value = parm0 + parm1;
+  console.log(value);
+}
+
+sum(7, 8);
+sum(78, 6);
+
+/*
+Write a function that takes two parameters then computes and logs the sum
+*/
