1b2fca254c33ceab7819f58772ca89738bd909a7
Eric Sowell <eric.sowell@gmail.com>
2016-11-07 14:24:48 -0600

    first lab

---
+++ labs/loops/lab-01.js
@@ -0,0 +1,15 @@
+var nums = [1, 6, 42, 3, 9];
+
+for (var i = 0; i < nums.length; i++) {
+  console.log(nums[i]);
+}
+
+/*
+Write a for loop loop and console.log the number out of the array. So there should be five logs each, each with those values. So...
+Expected output:
+1
+6
+42
+3
+9
+*/
