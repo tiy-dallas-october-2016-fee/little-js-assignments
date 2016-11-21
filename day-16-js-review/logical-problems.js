ed04c44e3934488d1e3a74084d9ef7f1d8e17771
Eric Sowell <eric.sowell@gmail.com>
2016-11-21 11:34:03 -0600

    numbered problems

--- day-16-js-review/logical-problems.js
+++ day-16-js-review/logical-problems.js
@@ -1,24 +1,23 @@
+//# 1 - write a function that takes two numbers and returns a sum
 
-//write a function that takes two numbers and returns a sum
+//# 2 - write a function that takes a password and returns if it is equal to 'p@55w0rd'
 
-//write a function that takes a password and returns if it is equal to 'p@55w0rd'
+//# 3 - write a function that takes a string and returns whether or not it is longer than 10 characters long.
 
-//write a function that takes a string and returns whether or not it is longer than 10 characters long.
+//# 4 - write a function that takes a string and returns whether or not it is longer than 10 characters long and contains an exclamation point.
 
-//write a function that takes a string and returns whether or not it is longer than 10 characters long and contains an exclamation point.
+//# 5 - write a function that takes an array of strings and returns the number of things in the array
 
-//write a function that takes an array of strings and returns the number of things in the array
+//# 6 - write a function that takes an array of numbers and returns the sum
 
-//write a function that takes an array of numbers and returns the sum
+//# 7 - write a function that takes an array and return `true` if the array contains more than 20 items.
 
-//write a function that takes an array and return `true` if the array contains more than 20 items.
+//# 8 - write a function that takes an array of strings and returns all the strings that contain the letter 'e'
 
-//write a function that takes an array of strings and returns all the strings that contain the letter 'e'
+//# 9 - write a function that takes an array of words and returns a new array with all the words uppercase
 
-//write a function that takes an array of words and returns a new array with all the words uppercase
+//# 10 - write a function that takes an array of strings and returns a count of the strings that do not contain vowels
 
-//write a function that takes an array of strings and returns a count of the strings that do not contain vowels
+//# 11 - write a function that takes an array of strings and returns a new string that contains only the words with vowels
 
-//write a function that takes an array of strings and returns a new string that contains only the words with vowels
-
-//write a function that takes an array of booleans and returns the number of true values
+//# 12 - write a function that takes an array of booleans and returns the number of true values
